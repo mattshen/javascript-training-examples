94515a26a311430c20f2fe38c59bba8da94c7485
Matt Shen <lovelywib@gmail.com>
2016-08-29 21:59:31 +1000

    Create iife_closure_data_privacy.js

---
+++ iife_closure_data_privacy.js
@@ -0,0 +1,22 @@
+// "counter" is a function that returns an object with properties, which in this case are functions.
+var counter = (function () {
+    var i = 0;
+
+    return {
+        get: function () {
+            return i;
+        },
+        set: function (val) {
+            i = val;
+        },
+        increment: function () {
+            return ++i;
+        }
+    };
+})();
+
+// These calls access the function properties returned by "counter".
+counter.get();  // 0
+counter.set(3);
+counter.increment();  // 4
+counter.increment();  // 5
