e3c447430d3aec15cd614c01abcadcb903c125b0
Matt Shen <lovelywib@gmail.com>
2016-09-01 09:24:24 +1000

    Update ex2.js

--- whatisthis/ex2.js
+++ whatisthis/ex2.js
@@ -3,3 +3,19 @@
 function sayThis() {
     alert(this == window);    // true
 }
+
+
+//some type of trap
+
+var name = 'Bob',
+person = {
+    name: 'Alice',
+    sayName: function() {
+        console.log(this === person);    // true
+        return function() {
+            console.log(this === person);    // false
+            console.log(this === window);    // true
+            console.log(this.name);          // Bob
+        };
+    }
+};
