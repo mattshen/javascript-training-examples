ca3d61813a1340dd2e4aac7cfbcf099d274b4ea5
Matt Shen <lovelywib@gmail.com>
2016-09-01 09:26:01 +1000

    Update ex2.js

--- whatisthis/ex2.js
+++ whatisthis/ex2.js
@@ -5,17 +5,33 @@ function sayThis() {
 }
 
 
-//some type of trap
-
+//a problem
 var name = 'Bob',
-person = {
-    name: 'Alice',
-    sayName: function() {
-        console.log(this === person);    // true
-        return function() {
-            console.log(this === person);    // false
-            console.log(this === window);    // true
-            console.log(this.name);          // Bob
-        };
-    }
-};
+    person = {
+        name: 'Alice',
+        sayName: function() {
+            console.log(this === person);    // true
+            return function() {
+                console.log(this === person);    // false
+                console.log(this === window);    // true
+                console.log(this.name);          // Bob
+            };
+        }
+    };
+
+
+//solution
+var name = 'Bob',
+    person = {
+        name: 'Alice',
+        sayName: function() {
+            console.log(this === person);    // true
+            return function() {
+                console.log(this === person);    // true
+                console.log(this === window);    // false
+                console.log(this.name);          // Alice
+            }.bind(this);
+        }
+    };
+
+person.sayName()();
